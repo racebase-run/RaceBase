1d3180a6877e5fcf1fe7c08962c5a6599f5dc52d
Remington Breeze <remington.breeze@gmail.com>
2019-02-04 12:24:12 -0800

    Built

--- client/.nuxt/App.js
+++ client/.nuxt/App.js
@@ -16,7 +16,7 @@ export default {
   head: {"title":"RaceBase - Community Sourced Running Results","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"name":"apple-mobile-web-app-title","content":"RaceBase"},{"property":"og:site_name","content":"RaceBase - Community Sourced Running Results"},{"name":"description","content":"For runners, by runners. We believe that people are more important than any race, or time, or championship. Join us now!"},{"property":"og:type","content":"website"},{"name":"google-site-verification","content":"geBi-OI3u5aN15krO_W0_IF9zr0d-N1NNcF1PMOhafk"}],"link":[{"rel":"icon","type":"image\u002Fpng","href":"\u002Fimages\u002Ffavicon-320.png"},{"rel":"apple-touch-icon","sizes":"180x180","href":"\u002Fimages\u002Ftouch-icon-iphone-retina.png"},{"rel":"apple-touch-icon","sizes":"167x167","href":"\u002Fimages\u002Ftouch-icon-iphone-retina.png"}],"script":[{"src":"https:\u002F\u002Fcdn.jsdelivr.net\u002Fnpm\u002Fjquery@3.2.1\u002Fdist\u002Fjquery.min.js"},{"src":"https:\u002F\u002Fcdnjs.cloudflare.com\u002Fajax\u002Flibs\u002Fpopper.js\u002F1.14.6\u002Fumd\u002Fpopper.min.js","crossorigin":"anonymous"},{"src":"https:\u002F\u002Fstackpath.bootstrapcdn.com\u002Fbootstrap\u002F4.2.1\u002Fjs\u002Fbootstrap.min.js"}],"style":[]},
 
   render(h, props) {
-    const loadingEl = h('nuxt-loading', { ref: 'loading' })
+    const loadingEl = h('NuxtLoading', { ref: 'loading' })
     const layoutEl = h(this.layout || 'nuxt')
     const templateEl = h('div', {
       domProps: {
@@ -50,6 +50,7 @@ export default {
     ])
   },
   data: () => ({
+    isOnline: true,
     layout: null,
     layoutName: ''
   }),
@@ -60,8 +61,12 @@ export default {
     // Add this.$nuxt in child instances
     Vue.prototype.$nuxt = this
     // add to window so we can listen when ready
-    if (typeof window !== 'undefined') {
+    if (process.client) {
       window.$nuxt = this
+      this.refreshOnlineStatus()
+      // Setup the listeners
+      window.addEventListener('online', this.refreshOnlineStatus)
+      window.addEventListener('offline', this.refreshOnlineStatus)
     }
     // Add $nuxt.error()
     this.error = this.nuxt.error
@@ -74,7 +79,25 @@ export default {
     'nuxt.err': 'errorChanged'
   },
 
+  computed: {
+    isOffline() {
+      return !this.isOnline
+    }
+  },
   methods: {
+    refreshOnlineStatus() {
+      if (process.client) {
+        if (typeof window.navigator.onLine === 'undefined') {
+          // If the browser doesn't support connection status reports
+          // assume that we are online because most apps' only react
+          // when they now that the connection has been interrupted
+          this.isOnline = true
+        } else {
+          this.isOnline = window.navigator.onLine
+        }
+      }
+    },
+
     errorChanged() {
       if (this.nuxt.err && this.$loading) {
         if (this.$loading.fail) this.$loading.fail()
@@ -83,8 +106,6 @@ export default {
     },
 
     setLayout(layout) {
-      if(layout && typeof layout !== 'string') throw new Error('[nuxt] Avoid using non-string value as layout property.')
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- client/.nuxt/index.js
+++ client/.nuxt/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Meta from 'vue-meta'
 import { createRouter } from './router.js'
-import NoSSR from './components/no-ssr.js'
+import NoSsr from './components/no-ssr.js'
 import NuxtChild from './components/nuxt-child.js'
-import NuxtLink from './components/nuxt-link.js'
 import NuxtError from './components/nuxt-error.vue'
 import Nuxt from './components/nuxt.js'
 import App from './App.js'
@@ -12,22 +11,22 @@ import { createStore } from './store.js'
 
 /* Plugins */
 
-import nuxt_plugin_googleanalytics_0a632f19 from 'nuxt_plugin_googleanalytics_0a632f19' // Source: ./google-analytics.js (ssr: false)
-import nuxt_plugin_templatesplugin6e691094_447c6c5e from 'nuxt_plugin_templatesplugin6e691094_447c6c5e' // Source: ./templates.plugin.6e691094.js
-import nuxt_plugin_bootstrapvue_7452b7c8 from 'nuxt_plugin_bootstrapvue_7452b7c8' // Source: ./bootstrap-vue.js
-import nuxt_plugin_axios_b1f5fa26 from 'nuxt_plugin_axios_b1f5fa26' // Source: ./axios.js
-import nuxt_plugin_axios_3566aa80 from 'nuxt_plugin_axios_3566aa80' // Source: ../plugins/axios
+import nuxt_plugin_googleanalytics_0a632f19 from 'nuxt_plugin_googleanalytics_0a632f19' // Source: ./google-analytics.js (mode: 'client')
+import nuxt_plugin_templatesplugin6e691094_447c6c5e from 'nuxt_plugin_templatesplugin6e691094_447c6c5e' // Source: ./templates.plugin.6e691094.js (mode: 'all')
+import nuxt_plugin_bootstrapvue_7452b7c8 from 'nuxt_plugin_bootstrapvue_7452b7c8' // Source: ./bootstrap-vue.js (mode: 'all')
+import nuxt_plugin_axios_b1f5fa26 from 'nuxt_plugin_axios_b1f5fa26' // Source: ./axios.js (mode: 'all')
+import nuxt_plugin_axios_3566aa80 from 'nuxt_plugin_axios_3566aa80' // Source: ../plugins/axios (mode: 'all')
 
-// Component: <no-ssr>
-Vue.component(NoSSR.name, NoSSR)
+// Component: <NoSsr>
+Vue.component(NoSsr.name, NoSsr)
 
-// Component: <nuxt-child>
+// Component: <NuxtChild>
 Vue.component(NuxtChild.name, NuxtChild)
+Vue.component('NChild', NuxtChild)
 
-// Component: <nuxt-link>
-Vue.component(NuxtLink.name, NuxtLink)
+// Component NuxtLink is imported in server.js or client.js
 
-// Component: <nuxt>`
+// Component: <Nuxt>`
 Vue.component(Nuxt.name, Nuxt)
 
 // vue-meta configuration
