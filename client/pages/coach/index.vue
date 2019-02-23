121c05bfc06ba519fbf7e888e7ccd58e4dd45914
Remington Breeze <remington.breeze@gmail.com>
2019-02-22 20:01:57 -0800

    Implemented semi-live version of coach dashboard with rostering functionality. Coming together

--- client/pages/coach/index.vue
+++ client/pages/coach/index.vue
@@ -1,65 +1,62 @@
 <style lang="less" scoped> 
 .profile-pic {
-  @size: 30px;
+  @size: 25px;
+  border-radius: 3px;
+  overflow: hidden;
   width: @size; height: @size;
 }
 </style>
 <template>
 <div class="mt-3 w-95 mx-auto">
-<h2> {{ team.name }} </h2>
-<h1> Current Roster </h1>
-<div class="roster mt-3">
-  <div v-for="athlete in team.roster" class="mb-1">
+<h2> {{ team.team_id }} </h2>
 
-    <div v-if="!athlete.athlete_id" class="d-flex align-items-center">
-      <div class="btn btn-outline-primary btn-small mr-2">
-        Invite
-      </div>
-      <div> {{ athlete.name }} </div>
+<div class="row"> 
+  <div class="col">
+    <h5> Athletes </h5>
+    <div v-for="athlete in athletes" class="d-flex align-items-center mb-1">
+      <ProfilePic v-if="athlete.profilePicUrl" class="profile-pic mr-2" :url="athlete.profilePicUrl" />
+      <div class="mr-2"> {{ athlete.athlete_id }} </div>
+      <div v-if="athlete.active" class="btn btn-outline-primary btn-small"> 
+        Add to Roster
+      </div> 
     </div>
+  </div>
 
-    <div class="d-flex align-items-center" v-else> 
-      <ProfilePic class="profile-pic mr-2" :url="athlete.profilePicUrl" />
-      <nuxt-link :to="'/athlete/' + athlete.athlete_id + '/logs'">
-        {{ athlete.name }}
-      </nuxt-link>
+  <div class="col">
+    <h5> Current Roster </h5>
+    <div v-for="athlete in team.roster">
+      {{ athlete.name }}
+      <div class=" ml-3 btn btn-outline-danger btn-small">Remove</div>
     </div>
-
   </div>
 </div>
+
 </div>
 </template>
 <script>
 const ProfilePic = () => import('~/components/User/ProfilePic')
 export default {
   components: { ProfilePic },
-  async asyncData ({ $axios }) {
-    let team = {
-      name: "Chico State", 
-      roster: [{
-        name: "Cameron Tomlinson", 
-        athlete_id: ""
-      },
-      {
-        name: "Matt Herrera", 
-        athlete_id: "mherrera"
-      },
-      {
-        name: "Remington Breeze", 
-        athlete_id: "remingtonbreeze"
-      }]
-    }
+  async asyncData ({ $axios, store }) {
 
-    for (const athlete of team.roster) {
-      if (athlete.athlete_id) {
-        let data = await $axios.$get('/user/' + athlete.athlete_id + '/profilePic')
-        if (data.url) athlete.profilePicUrl = data.url
-        else athlete.profilePicUrl = null       
-      }
+    let user = { ...store.state.auth.user }
+
+    let team = await $axios.$get('/team/' + user.team_id)
+    let users = await $axios.$get('/team/' + user.team_id + '/users')
+    let data = await $axios.$get('/team/' + user.team_id + '/athletes')
+
+    let athletes = []
+
+    for (const athlete of data) {
+      let a = await $axios.$get('/user/athlete/' + athlete)
+      a.athlete_id = athlete
+      athletes.push(a)
     }
 
     return {
-      team: team
+      team: team, 
+      athletes: athletes, 
+      roster: []
     }
   }
 }
