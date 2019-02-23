0e26651575b9ff6b73a8cd6443f285a61d9bbeed
Remington Breeze <remington.breeze@gmail.com>
2019-02-22 16:08:30 -0800

    Stylized athlete's log monthly view

--- client/pages/athlete/_id/logs.vue
+++ client/pages/athlete/_id/logs.vue
@@ -1,29 +1,129 @@
 <style lang="less" scoped>
-.week, .labels {
-  border-bottom: 1px solid black;
+@import (reference) '~assets/less/colors.less';
+
+.shoe {
+  width: 25px;
 }
-.labels .col {
+
+.labels {
+  border-bottom: 1px solid @ultra-light-grey;
+  .col {
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 15px;
+  }
+}
+
+.sleep {
   text-align: center;
+  font-size: 15px;
+  font-weight: 500;
+  .fa-bed {
+    font-size: 14px;
+    margin-right: 2px;
+    color: @orange;
+  }
 }
+
+.week, .week > .col {
+  border-style: solid;
+  border-color: @ultra-light-grey;
+}
+.week        { border-width:  0px  0   0    1px }
+.week > .col { border-width:  0    1px 1px  0   }
+
+.week {
+
+  .mileage {
+    font-size: 24px;
+    color: @bright-blue;
+    text-align: center;
+    font-weight: 500;
+  }
+
+  label {
+    text-transform: uppercase;
+    font-weight: 400;
+    font-size: 13px;
+    color: @label-grey;
+    margin-bottom: 0px;
+  }
+
+  .pace {
+    text-align: center;
+    color: @bright-blue;
+  }
+
+  .run-count {
+    font-size: 15px;
+    text-align: center; 
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+
+  .dom {
+    position: absolute;
+    right: 10px;
+    top: 5px;
+    font-weight: 500;
+    font-size: 15px;
+    text-align: right;
+    &.today {
+      text-align: center;
+      border-radius: 100px;
+      color: white;
+      background: @pink;
+      padding-bottom: 0px;
+      @size: 27px;
+      line-height: @size;
+      width: @size;
+      height: @size;
+    }
+  } 
+}
+
+
+
 </style>
 <template>
 <div class="mt-4 mx-auto w-95"> 
   <h4> {{ athlete.name }}'s Logs </h4>
-  <div> {{ athlete.trainer }}</div>
-  <div> {{ athlete.team_id }}</div>
-  <div class="labels row"> 
-    <div class="col" v-for="day in daysOfWeek"> 
-      <strong>{{ day }}</strong>
-    </div>
+  <div> 
+    <object type="image/svg+xml" data="/images/trainer-blue.svg" class="shoe mr-1">
+    </object>
+    {{ athlete.trainer }}
   </div>
-  <div class="week row" v-for="week in weeks">
-    <div class="col" v-for="entry in week"> 
-      <strong>{{ entry.dom }}</strong>
-      <div v-for="run in entry.runs">
-        {{ run.distance }} at {{ getPace(run.time, run.distance) }}
+  <div class="calendar container-fluid mt-3 mb-5">
+    <div class="labels row py-2"> 
+      <div class="col" v-for="day in daysOfWeek"> 
+        <strong>{{ day }}</strong>
       </div>
-      Slept {{ entry.sleep || "?" }} hrs
-    </div> 
+    </div>
+    <div class="week row" v-for="week in weeks">
+      <div class="col p-3" v-for="entry in week"> 
+        <div class="dom ml-auto" :class="entry.today ? 'today' : ''">
+          {{ entry.dom }}
+        </div>
+
+        <div class="mileage mt-2" v-if="entry.totalMileage > 0"> 
+          {{ entry.totalMileage || "0" }}
+          <label> mi </label>
+        </div>
+
+        <div class="pace mb-1" v-if="entry.totalMileage > 0">
+          {{ entry.avgPace || "0:00" }}
+          <label> min / mi </label>
+        </div>
+
+        <div class="run-count" v-if="entry.runs && entry.totalMileage > 0">
+          <div class="mb-2" v-if="entry.runs.length > 1"> {{ entry.runs.length }} runs</div>
+        </div>
+
+        <div v-if="entry.sleep" class="sleep"> 
+          <fa icon="bed"></fa> {{ entry.sleep }} hrs
+        </div>
+      </div> 
+    </div>
   </div>
 </div>
 </template>
@@ -35,6 +135,26 @@ let {
   getDateFromUrl, 
   getPace 
 } = require('~/utils/date.js')
+
+let roundToHundredths = function(n) { return Math.round(n * 100) / 100 }
+let sumRuns = function(runs) {
+  if (!runs) return { totalMileage: 0, totalElev: 0, totalTime: "0:00" }
+  var totalMileage = 0
+  var totalElev = 0
+  var totalTime = 0
+
+  for (var i = 0, l = runs.length; i < l; i++ ) {
+    totalMileage += runs[i].distance || 0
+    totalElev += runs[i].elevationGain || 0
+    totalTime += timeStringToDecimal(runs[i].time)
+  }
+  return { 
+    totalMileage: roundToHundredths(totalMileage), 
+    totalElev: roundToHundredths(totalElev), 
+    totalTime: timeDecimalToString(roundToHundredths(totalTime))
+  }
+}
+
 import moment from 'moment'
 export default {
   async asyncData ({ params, $axios, redirect }) {
@@ -56,17 +176,31 @@ export default {
     // create an array of 4 weeks
     let weeks = Array.apply(null, Array(4)).map((_, i) => {
       // get the start of each week
-      let startOfWeek = moment(getDateFromUrl(params.date)).subtract(7*i, 'days').startOf('isoWeek')
+      let endOfWeek = moment().subtract(7*i, 'days').endOf('isoWeek')
 
       // find corresponding data from entries and put it in its appropriate spot
       let week = Array.apply(null, Array(7)).map((_, j) => {
-        let day = moment(startOfWeek).subtract(j, 'days')
+        let day = moment(endOfWeek).subtract(j, 'days')
         let date = day.format("DD MM YYYY")
         let dom = day.format("DD")
 
+        // mark the entry with the day of the month
         let entry = entries.find(entry => moment(entry.date).format("DD MM YYYY") == date) || {}
-
         entry.dom = dom
+
+        // set date for each entry 
+        entry.date = day.toDate()
+
+        // sum all runs on the entry
+        if (entry.runs) {
+          let sums = sumRuns(entry.runs)
+          entry.totalMileage = sums.totalMileage
+          entry.avgPace = getPace(sums.totalTime, sums.totalMileage)
+        }
+
+        // mark today's entry
+        if (day.format("DD MM YYYY") == moment().format("DD MM YYYY")) 
+          entry.today = true
 
         return entry
       })
@@ -85,7 +219,15 @@ export default {
   methods: { 
     getPace: function(time, distance) {
       return getPace(time, distance)
-    }
+    },
+    weeklyMileage: function(week) {
+      var total = 0
+      for (var i = 0, l = week.length; i < l; i++ ) {
+        if (week[i].totalMileage)
+          total += week[i].totalMileage || 0
+      }
+      return Math.round(total * 100) / 100
+    } 
   }
 }
 </script>
