49634b50fef10acd0a5ce6a92bc0c6eedd3540e8
Remington Breeze <remington.breeze@gmail.com>
2019-02-24 16:24:36 -0800

    Implemented route for joining team by url

---
+++ client/pages/user/join/_joincode.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="mt-3 ml-4"> 
+  <h2> {{ message }}! </h2>
+  <div>Click <nuxt-link to="/login">here</nuxt-link> to log in</div>
+</div>
+</template>
+<script> 
+export default {
+  async asyncData ({ $axios, redirect, params }) {
+    let res = await $axios.$post('team/join/' + params.joincode)
+    let message
+    if (res == "Joined team")
+      message = "Successfully joined team"
+    else 
+      message = "Something went wrong"
+    return {
+      message: message
+    }
+  }
+}
+</script>
