b69a05f346de659eb69774b964d9d0118fe40240
Remington Breeze <remington.breeze@gmail.com>
2019-03-13 20:38:47 -0700

    Implemented Pace Zone Calculator. Spent the past like 3 days on Desmos and my TI89 coming up with equations for estimating zones based on PR's. This draft gets pretty close

--- client/pages/tools/pacezones.vue
+++ client/pages/tools/pacezones.vue
@@ -1,13 +1,39 @@
 <style lang="less" scoped>
+@import (reference) "~assets/less/colors.less";
 
-th {
-  td {
+thead {
+  th {
     font-weight: 500; 
     border-right: 1px solid black; 
     text-align: center;
     padding: 0px 10px;
   }
 }
+
+td {
+  text-align: center;
+}
+
+.slider {
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  background: @light-grey + #111;
+  border-radius: 20px;
+  height: 7px;
+}
+
+.slider::-webkit-slider-thumb, .slider::-moz-range-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 15px;
+  height: 15px;
+  border-radius: 100px;
+  background: @bright-blue;
+  border: none;
+  cursor: pointer;
+}
+
 </style>
 <template>
 <div class="mx-auto w-95 mt-4">
@@ -15,38 +41,119 @@ th {
   <p> Enter your PR for any distance below: </p>
   <div class="container my-4">
     <div class="row">
-      <input type="text" placeholder="Finishing Time" class="col mr-3 form-control"/>
-      <input type="number" placeholder="Distance" class="col form-control"> 
+      <input type="text" placeholder="Finishing Time" class="col mr-3 form-control" v-model="input.time"/>
+      <input type="number" placeholder="Distance" class="col form-control" v-model="input.distance"/> 
     </div>
   </div>
-  <table>
+  <div class="mb-3"> PR Pace: {{ pace }}</div>
+  <table class="table">
     <thead> 
-      <th> 
-        <td> Recovery / Regen Pace </td>
-        <td> Aerobic Pace </td>
-        <td> Marathon Pace </td>
-        <td> Long Tempo Pace </td>
-        <td> Lactate Threshold (LT / ~10 mi RP ) </td>
-        <td> Critical Velocity (CV / 10k RP) </td>
-        <td> 5k RP </td>
-        <td> 3k RP </td>
-        <td> 1500m RP </td>
-      </th>
+      <tr> 
+        <th v-for="zone in zones"> 
+          {{ zone.name }}
+        </th>
+      </tr>
     </thead>
     <tbody>
       <tr> 
-        <td> </td>
-        <td>  </td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
-        <td> </td>
+        <td v-for="(zone, index) in zones">
+          {{ calculatePace(zone.range[0]) }} - {{ zone.range[1] ? calculatePace(zone.range[1]) : 'INF' }}
+        </td>
       </tr>
     </tbody>
   </table>
+  
+  <div> {{ calculatePace(curDist) }} - {{ getZone(curDist) }} </div>
+  <div class="slider mt-3">
+    <input 
+      type="range" 
+      :min="0" :max="13" 
+      :step="0.2"
+      class="slider mb-2"
+      v-model="scaleVal"
+    />
+  </div>
 
 </div>
 </template>
+
+<script> 
+// human minimum pace
+const hl = 2.57; 
+
+// constants
+const o = 160; 
+const a = 30.5;
+const u = 1.46;
+
+// logarithm function
+function logBase(x, y) {
+  return Math.log(y) / Math.log(x);
+}
+
+import { timeStringToDecimal, timeDecimalToString, getPace } from '~/utils/date'
+export default {
+  data () {
+    return {
+      zones: [
+        { name: 'Anaerobic', range: [0, 2.7] },
+        { name: 'V02', range: [2.7, 5] }, 
+        { name: 'CV', range: [5, 8.4] }, 
+        { name: 'LT', range: [8.4, 14.0] }, 
+        { name: 'AT', range: [14.0, 43.0] }, 
+        { name: 'Aerobic', range: [43.0, 95.0] }, 
+        { name: 'Recovery', range: [95.0, null] }
+      ],
+      input: {
+        time: "", 
+        distance: 0
+      }, 
+      scaleVal: 0
+    }
+  },
+  methods: {
+    slowDown: function(dist) {
+      return (1 + (dist/o)) * logBase(a, this.k*dist + 1)
+    },
+    calculatePace: function(dist) {
+      return timeDecimalToString(this.paceLimit + this.slowDown(dist))
+    }, 
+    calculatePaceDecimal: function(dist) {
+      return this.paceLimit + this.slowDown(dist)
+    },
+    getZone: function(dist) {
+      for (const zone of this.zones) {
+        let min = zone.range[0]
+        let max = zone.range[1]
+        if (dist >= min && dist < max) return zone.name
+      }
+      return "Recovery"
+    }
+  }, 
+  computed: {
+    timeDecimal: function() {
+      return timeStringToDecimal(this.input.time) 
+    }, 
+    pace: function() {
+      return getPace(this.input.time, this.input.distance)
+    },
+    paceDecimal: function() {
+      return timeStringToDecimal(this.pace)
+    },
+    k: function() {
+      let d = Number(this.input.distance)
+      let l = this.paceLimit
+      let p = this.paceDecimal
+      let exp = Math.pow( a, (p - l) / ((d / o) + 1) )
+      return (exp - 1) / d
+    }, 
+    paceLimit: function() {
+      let z = Math.pow(u, this.paceDecimal)
+      return z / (Number(this.input.distance) + (z / (this.paceDecimal - hl))) + hl
+    }, 
+    curDist: function() {
+      return Math.pow(this.scaleVal, 1.83)
+    }
+  }
+}
+</script>
